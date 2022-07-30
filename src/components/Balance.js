afd70b131a5c3d1d89606a03d571879f55bf72f9
manuel.ochoa <manuel.ochoa@eydevelopment.com>
2022-07-30 23:08:40 +0200

    replace hardcoded total by useContext globalState, use reduce acumulator

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,10 +1,14 @@
-import React from 'react'
-
+import React, {useContext} from 'react'
+import { GlobalContext } from '../context/GlobalState'
 const Balance = () => {
+  const {transactions} = useContext(GlobalContext)
+  const amounts = transactions.map(transaction => transaction.amount)
+  const total = amounts.reduce((acc,item) => (acc+=item), 0).toFixed(2)
+  
   return (
     <>
         <h4>Income</h4>
-        <p id="money-plus" className="money plus">+$0.00</p>
+        <p id="money-plus" className="money plus">${total}</p>
     </>
   )
 }
