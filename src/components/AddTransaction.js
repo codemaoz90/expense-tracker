a01e90ffc31c76b55124cd0863bcd13f5989d4ce
manuel.ochoa <manuel.ochoa@eydevelopment.com>
2022-07-31 22:09:46 +0200

    Add form actions

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,12 +1,24 @@
-import React, { useState } from 'react'
-
+import React, { useState, useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
 function AddTransaction() {
+  const {addTransaction} = useContext(GlobalContext)
   const [text, setText] = useState("")
   const [amount, setAmount] = useState(0)
+
+  const onSubmit = e => {
+    e.preventDefault()
+    const newTrasaction = {
+      id: Math.floor(Math.random() * 100000000),
+      text,
+      amount: parseInt(amount)
+    }
+    addTransaction(newTrasaction)
+  }
+
   return (
     <>
         <h3>Add new transaction</h3>
-        <form id="form">
+        <form id="form" onSubmit={onSubmit}>
             <div className="form-control">
             <label htmlFor="text">Text</label>
             <input type="text" placeholder="Enter text..." value={text} onChange={(e) => setText(e.target.value)} />
@@ -17,7 +29,7 @@ function AddTransaction() {
             </label>
             <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..." />
             </div>
-            <button className="btn">Add transaction</button>
+            <button className="btn" type="submit">Add transaction</button>
         </form>
     </>
   )
--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
 function Transaction({transaction}) {
+  const {deleteTransaction} = useContext(GlobalContext)
   const sign = transaction.amount < 0 ? '-' : '+'
   return (
     <li className="minus" >
-    {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span><button className="delete-btn">x</button>
+    {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span>
+    <button className="delete-btn" onClick={() => deleteTransaction(transaction.id)}>x</button>
     </li>
   )
 }
